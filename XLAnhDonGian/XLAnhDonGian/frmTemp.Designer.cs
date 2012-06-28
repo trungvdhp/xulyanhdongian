44631c98891f5465f2f56e484d84611f1d546734
trungvdt49 <trungvdt49@61fc7781-fe07-90f6-58b6-34cc18f9da9e>
2012-06-28 01:41:24 +0000

    Phien ban dau tien

---
+++ XLAnhDonGian/XLAnhDonGian/frmTemp.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace XLAnhDonGian
+{
+    partial class frmTemp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btANPhan = new System.Windows.Forms.Button();
+            this.btBlue = new System.Windows.Forms.Button();
+            this.btGreen = new System.Windows.Forms.Button();
+            this.btRed = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btAXam = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(285, 136);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(261, 317);
+            this.panel2.TabIndex = 23;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 136);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(253, 320);
+            this.panel1.TabIndex = 22;
+            // 
+            // btANPhan
+            // 
+            this.btANPhan.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btANPhan.Location = new System.Drawing.Point(415, 462);
+            this.btANPhan.Name = "btANPhan";
+            this.btANPhan.Size = new System.Drawing.Size(110, 23);
+            this.btANPhan.TabIndex = 21;
+            this.btANPhan.Text = "Ảnh Nhị Phân";
+            this.btANPhan.UseVisualStyleBackColor = true;
+            // 
+            // btBlue
+            // 
+            this.btBlue.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btBlue.Location = new System.Drawing.Point(316, 462);
+            this.btBlue.Name = "btBlue";
+            this.btBlue.Size = new System.Drawing.Size(75, 23);
+            this.btBlue.TabIndex = 20;
+            this.btBlue.Text = "Blue";
+            this.btBlue.UseVisualStyleBackColor = true;
+            // 
+            // btGreen
+            // 
+            this.btGreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btGreen.Location = new System.Drawing.Point(219, 462);
+            this.btGreen.Name = "btGreen";
+            this.btGreen.Size = new System.Drawing.Size(75, 23);
+            this.btGreen.TabIndex = 19;
+            this.btGreen.Text = "Green";
+            this.btGreen.UseVisualStyleBackColor = true;
+            // 
+            // btRed
+            // 
+            this.btRed.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btRed.Location = new System.Drawing.Point(125, 462);
+            this.btRed.Name = "btRed";
+            this.btRed.Size = new System.Drawing.Size(75, 23);
+            this.btRed.TabIndex = 18;
+            this.btRed.Text = "Red";
+            this.btRed.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(386, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 15);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Kết Quả";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(91, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Ảnh Gốc";
+            // 
+            // btAXam
+            // 
+            this.btAXam.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAXam.Location = new System.Drawing.Point(31, 462);
+            this.btAXam.Name = "btAXam";
+            this.btAXam.Size = new System.Drawing.Size(75, 23);
+            this.btAXam.TabIndex = 17;
+            this.btAXam.Text = "Ảnh Xám";
+            this.btAXam.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(137, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 24);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Chương trình xử lý ảnh cơ bản";
+            // 
+            // frmTemp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 554);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btANPhan);
+            this.Controls.Add(this.btBlue);
+            this.Controls.Add(this.btGreen);
+            this.Controls.Add(this.btRed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btAXam);
+            this.Controls.Add(this.label1);
+            this.Name = "frmTemp";
+            this.Text = "frmTemp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btANPhan;
+        private System.Windows.Forms.Button btBlue;
+        private System.Windows.Forms.Button btGreen;
+        private System.Windows.Forms.Button btRed;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btAXam;
+        private System.Windows.Forms.Label label1;
+    }
+}
